5757b7c8230ebdb2aed1d14f32439fc5c9f984f5
picker770 <arehman897@gmail.com>
2025-03-10 11:48:05 +0000

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-ECHO is on.
+// ECHO is on.
+
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle('active')
+  })
+})
